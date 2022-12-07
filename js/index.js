47f9137fc98dc951f2584d0360c08a833ecd4001
SvetlanaBeynik <beynik.svetlana@gmail.com>
2022-12-07 11:39:31 -0500

    <message>

--- js/index.js
+++ js/index.js
@@ -15,3 +15,34 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+const messageForm = document.querySelector('[name="leave_message"]');
+
+
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const message = event.target.message.value;
+
+    console.log(name);
+    console.log(email);
+    console.log(message);
+
+    messageSection = document.querySelector("#messages");
+    messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a> <span>${message}</span> </a>`;
+    messageList.appendChild(newMessage);
+    removeButton = document.createElement('button')
+    removeButton.innerText = `Remove`;
+    removeButton.setAttribute("type", "button");
+    removeButton.addEventListener('click', (event) => {
+        entry = removeButton.parentNode;
+        entry.remove();
+    })
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    event.target.reset();
+})
